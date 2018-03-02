2beb2a65e5cdfcff507b00275d565c19cfeef8b2
toluo7449 <toluo7449@7J2M6Y1.Student.UGDSB.ED>
2018-03-02 13:48:17 -0500

    my second commit

---
+++ src/AreaofARectangle.java
@@ -0,0 +1,22 @@
+/*
+ * Sean Luo
+ * AreaOfRectangle.java
+ * This program calculates the AreaOfRectangle 
+ */
+public class AreaofARectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double length=5.7;
+        double width=4.8;
+        double area=length*width;
+        System.out.println("The length is= "+length);
+        System.out.println("The width is= "+width);
+        System.out.println("The area is= "+area);
+                
+        
+    }
+    
+}
